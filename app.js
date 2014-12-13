a84b676f919c75da55fc1a4cec8ddec88a39f20d
pimlu <pimlu@users.noreply.github.com>
2014-12-12 22:58:56 -0500

    prepare for http-proxy
    sigh

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ var
   debug=require('./debug.js');
 
 var frontw=config.front.workers,
-  cdw=config.cd.workers,
   worldw=_.size(config.game.worlds);
 if(cluster.isMaster) {
   debug=debug('blue','master');
   debug('opening %s front workers, '+
-  '%s cd workers, %s world workers',frontw,cdw,worldw);
+  '%s world workers',frontw,worldw);
   require('./dbcheck.js')(knex,debug).then(function() {
-    for(var i=0;i<frontw+cdw+worldw;i++) {
+    for(var i=0;i<frontw+worldw;i++) {
       cluster.fork();
     }
   });
@@ -23,13 +22,9 @@ if(cluster.isMaster) {
     debug=debug('green','front',id)
     debug('initializing');
     require('./front/front.js')(knex,debug);
-  } else if(id<=frontw+cdw) {
-    debug=debug('red','cd',id-frontw);
-    debug('initializing');
-    require('./cd/cd.js')(debug);
   } else {
     //loop through world keys (the world number) until the IDth key
-    var i=frontw+cdw;
+    var i=frontw;
     for(var wnum in config.game.worlds) {
       if(++i===id) break;
     }
