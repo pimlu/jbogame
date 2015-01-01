f429309798a9b6098354e1968ac9e38d17e54cbd
pimlu <pimlu@users.noreply.github.com>
2014-12-31 23:49:47 -0500

    create body definition function, alpha centauri exoplanet
    happy new year and all that.  this thing has too many arguments; I should make it an options object instead.  the alpha centauri A exoplanet is completely imaginary lol, I think they're only even considering one around B at the moment

--- master/builder.js
+++ master/builder.js
@@ -4,31 +4,44 @@ var config=require('../config.js');
 var
   au=149597870700,//m
   pc=3.26163344,//ly
-  re=6378100;//m
+  re=6378100,//m
+  ld=384400*1000,//m
+  lr=1737.10*1000;//m
 module.exports=function(debug,knex) {
   debug('build');
+
+  function body(system,name,rocky,r,x,y,z,parent) {
+    return knex('bodies').returning('id').insert({
+      r:r,
+      rocky:rocky
+    }).then(function(bodyid) {
+      var columns={
+        systemid:system[0],
+        name:name,
+        orbiting:true,
+        x:x,y:y,z:z,
+        bodyid:bodyid[0]
+      };
+      if(parent) columns.parentid=parent[0];
+      return knex('places').returning('id').insert(columns);
+    });
+  }
+
   knex('systems').returning('id').insert({
     name:'sol',
     loadavg:1,
     x:0,y:0,z:0
   }).then(function(id) {
-    return knex('bodies').returning('id').insert({
-      r:re,
-      rocky:true
-    }).then(function(bodyid) {
-      return knex('places').insert({
-        systemid:id[0],
-        name:'earth',
-        orbiting:true,
-        x:au,y:0,z:0,
-        bodyid:bodyid[0]
-      });
+    return body(id,'earth',true,re,au,0,0).then(function(bodyid) {
+      return body(id,'moon',true,lr,ld,0,0,bodyid);
     });
   }).then(function() {
-    return knex('systems').insert({
+    return knex('systems').returning('id').insert({
       name:'alpha centauri',
       loadavg:0.5,
       x:4,y:0,z:0
-    })
+    });
+  }).then(function(id) {
+    body(id,'alpha centauri Ab',true,re*4,au*1.2,0,0);
   });
 };
